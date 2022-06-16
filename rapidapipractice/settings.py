a233b2d6680f0d8cd25d449b5b949b42b664a0be
piyush <piyush.kansal@myclassroom.digital>
2022-06-16 16:06:05 +0530

    commit

--- rapidapipractice/settings.py
+++ rapidapipractice/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rapidapipractice.api',
+    'api.apps.ApiConfig',
     'rest_framework'
 ]
 
